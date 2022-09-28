b752b7e216d846e87f30bfdd9de8f67c82829b6d
RParkerYules <109665209+RParkerYules@users.noreply.github.com>
2022-09-28 23:52:07 +1000

    Added the rest of the questions

--- Develop/index.js
+++ Develop/index.js
@@ -6,8 +6,58 @@ const generateMarkdown = require('./utils/generateMarkdown')
 // This is the first question
 const questions = [{
     type: 'input', 
-    name: 'github-user-name',
-    message: 'What is your GitHub username?'
+    name: 'Project Title',
+    message: 'What is your Project Title?'
+},
+{
+    type: 'input', 
+    name: 'GitHub Username',
+    message: 'What is your GitHub Username?'
+},
+{
+    type: 'input', 
+    name: 'Email Address',
+    message: 'What is your email address?'
+},
+{
+    type: 'input', 
+    name: 'Description',
+    message: 'Write a short description for your project.'
+},
+{
+    type: 'input', 
+    name: 'License(s)',
+    message: 'What licenses should your project have?',
+    choices: [
+        'Apache license 2.0',
+        'BSD 2-clause "Simplified" license',
+        'BSD 3-clause "New" or "Revised" license',
+        'GNU General Public License family',
+        'GNU Library or "Lesser" General Public License (LGPL)',
+        'MIT',
+        'Mozilla Public License 2.0',
+    ],
+    default: "MIT"
+},
+{
+    type: 'input', 
+    name: 'Dependencies',
+    message: 'What command should be run to install dependencies?'
+},
+{
+    type: 'input', 
+    name: 'Tests',
+    message: 'What command should be run to run tests?'
+},
+{
+    type: 'input', 
+    name: 'User Information',
+    message: 'What does the user need to know about using the repo?'
+},
+{
+    type: 'input', 
+    name: 'Contributors',
+    message: 'What does the user need to know about contributing to the repo?'
 },
 
 ]
@@ -19,7 +69,7 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then(answers=>{
+    inquirer.prompt(questions) .then(answers => {
         console.log(answers)
 
         writeToFile('README.md', generateMarkdown(answers))
@@ -28,4 +78,11 @@ function init() {
 
 // Function call to initialize app
 init()
+
+
+
+
+
+
+
 
